73e9e4996ae4e3bdb4475e2897dc168de9256212
Andre Gois <andre-gois@hotmail.com>
2018-10-25 16:18:49 -0300

    Adição da tela novo

--- app/src/main/java/com/desenvolvimentoandroid/andregois/testegit/novoActivity.java
+++ app/src/main/java/com/desenvolvimentoandroid/andregois/testegit/novoActivity.java
@@ -3,13 +3,14 @@ package com.desenvolvimentoandroid.andregois.testegit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class novoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_novo);
 
+        //esse é apenas um teste
 
     }
 }
