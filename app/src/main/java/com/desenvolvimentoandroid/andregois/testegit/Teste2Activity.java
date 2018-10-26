3882ded4513bcbdf9c48f1629a9fde9f96c113b3
Andre Gois <andre-gois@hotmail.com>
2018-10-25 21:57:24 -0300

    Adição da tela teste2

---
+++ app/src/main/java/com/desenvolvimentoandroid/andregois/testegit/Teste2Activity.java
@@ -0,0 +1,13 @@
+package com.desenvolvimentoandroid.andregois.testegit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teste2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste2);
+    }
+}
